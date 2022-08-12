de8960de7b5dbd1fac2d18a5871232e7d45315f5
Sharon John <36360017+23sharon@users.noreply.github.com>
2022-08-13 01:17:34 +0530

    Add files via upload

---
+++ ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int negNum =-7;
+		int posNum =3;
+		int result= 0;
+		if(negNum<0)
+		{
+			result =negNum*-1;
+			System.out.println(result+" conversion of Positiveto negative");
+		}
+	}
+
+}
